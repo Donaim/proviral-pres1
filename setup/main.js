06ebb2195431a187d45cc86e0519d9c9009dfd52
Vitaliy Mysak <vmysak@bccfe.ca>
2024-10-01 17:03:37 -0700

    save

--- setup/main.js
+++ setup/main.js
@@ -1,22 +1,8 @@
 
-import { defineConfig } from 'vite'
-import { AccordionRoot, AccordionItem, AccordionTrigger, AccordionHeader, AccordionContent } from 'radix-vue';
+// import { Accordion, AccordionItem, AccordionTrigger, AccordionHeader, AccordionContent } from 'radix-vue';
 
 const hello = function () {
     console.log("My setup here.");
 }
 
-export default defineConfig({
-    // slidev: {
-    //     vue: {
-    //         setup() {
-    //             // Register components globally
-    //             app.component('AccordionRoot', AccordionRoot);
-    //             app.component('AccordionItem', AccordionItem);
-    //             app.component('AccordionTrigger', AccordionTrigger);
-    //             app.component('AccordionHeader', AccordionHeader);
-    //             app.component('AccordionContent', AccordionContent);
-    //         },
-    //     },
-    // },
-});
+export default hello;

6fcce78325a4ca331ed27ea9e7d8b7b63af660d8
Vitaliy Mysak <vmysak@bccfe.ca>
2024-10-01 17:23:58 -0700

    save

--- setup/main.js
+++ setup/main.js
@@ -2,7 +2,7 @@
 // import { Accordion, AccordionItem, AccordionTrigger, AccordionHeader, AccordionContent } from 'radix-vue';
 
 import { AccordionContent, AccordionHeader, AccordionItem, AccordionRoot, AccordionTrigger } from 'radix-vue';
-import './components/styles.css'
+import '../components/styles.css'
 
 const hello = function () {
     console.log("My setup here.");

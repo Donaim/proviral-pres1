e5dc155da5673f8e251afad3b299048f2ac96072
Vitaliy Mysak <vmysak@bccfe.ca>
2024-10-04 11:32:08 -0700

    save

---
+++ components/SlideProgress.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { AccordionContent, AccordionHeader, AccordionItem, AccordionRoot, AccordionTrigger } from 'radix-vue'
+import './styles.css'
+</script>
+
+<div class="absolute bottom-0 left-0 right-0">
+  <SlideCurrentNo  /> / <SlidesTotal />
+</div>

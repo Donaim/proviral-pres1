88dbf3cd8a40d3e1c1173ed7570bfd6d9ac47e10
Vitaliy Mysak <vmysak@bccfe.ca>
2024-10-04 11:34:54 -0700

    save

--- components/SlideProgress.vue
+++ components/SlideProgress.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { SlideCurrentNo, SlidesTotal } from '@slidev/components'
+import { SlideCurrentNo, SlidesTotal } from '@slidev/client/builtin'
 </script>
 
-<div class="absolute bottom-0 left-0 right-0">
-  <SlideCurrentNo  /> / <SlidesTotal />
-</div>
+<template>
+  <div class="absolute bottom-0 left-0 right-0">
+     <SlideCurrentNo  /> / <SlidesTotal />
+  </div>
+</template>

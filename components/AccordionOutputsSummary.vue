7c0bc50bf9bc216e7201987b2df4d2860729c10a
Vitaliy Mysak <vmysak@bccfe.ca>
2024-10-03 17:34:58 -0700

    save

--- components/AccordionOutputsSummary.vue
+++ components/AccordionOutputsSummary.vue
@@ -5,93 +5,97 @@ import './styles.css'
 
 <template>
   <AccordionRoot
-
     class="AccordionRoot"
     default-value="'item-1'"
     type="single"
     :collapsible="true"
+  >
 
-    @keydown.stop.prevent
-    @keypress.stop.prevent
-    @keyup.stop.prevent
+    <AccordionItem class="AccordionItem" value='item-1'>
+      <AccordionHeader class="AccordionHeader">
+        <AccordionTrigger class="AccordionTrigger">
+          <span>defects.csv</span>
+        </AccordionTrigger>
+      </AccordionHeader>
+      <AccordionContent class="AccordionContent">
+        <div class="AccordionContentText">
+          Detailed description of every defect detected by CFEIntact. 
+          <a href="https://github.com/cfe-lab/CFEIntact/blob/master/tests/expected-results-small-csv/defects.csv" target="_blank">Example.</a>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
 
-    >
+    <AccordionItem class="AccordionItem" value='item-2'>
+      <AccordionHeader class="AccordionHeader">
+        <AccordionTrigger class="AccordionTrigger">
+          <span>landscapes.csv</span>
+        </AccordionTrigger>
+      </AccordionHeader>
+      <AccordionContent class="AccordionContent">
+        <div class="AccordionContentText">
+          Data used to plot the defect landscapes on BBLabTools. 
+          <a href="https://gist.github.com/Donaim/3371ccb218270be00af466beb771a4ab" target="_blank">Example.</a>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
 
-<AccordionItem class="AccordionItem" value='item-1'>
-  <AccordionHeader class="AccordionHeader">
-    <AccordionTrigger class="AccordionTrigger">
-      <span>
-        Handling Clustered X's
-      </span>
-    </AccordionTrigger>
-  </AccordionHeader>
-  <AccordionContent class="AccordionContent">
-    <div class="AccordionContentText">
-      If there are gaps (<b>X</b>'s) in the sequence, the pipeline
-      attempts to remove them if they are <u>clustered near the
-      ends</u>. If the <b>X</b>'s cannot be removed, the sequence is
-      skipped for primer detection in that region.
-    </div>
-  </AccordionContent>
-</AccordionItem>
+    <AccordionItem class="AccordionItem" value='item-3'>
+      <AccordionHeader class="AccordionHeader">
+        <AccordionTrigger class="AccordionTrigger">
+          <span>landscapes.svg</span>
+        </AccordionTrigger>
+      </AccordionHeader>
+      <AccordionContent class="AccordionContent">
+        <div class="AccordionContentText">
+          Landscape plot of sequences' defects. 
+          <a href="https://raw.githubusercontent.com/gist/Donaim/aa2b1919ff521ab30b47c09924c365e9/raw/77bcdec7fb3fb1bab4879c72886dbd154b7e011a/landscape.svg" target="_blank">Example.</a>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
 
-<AccordionItem class="AccordionItem" value='item-2'>
-  <AccordionHeader class="AccordionHeader">
-    <AccordionTrigger class="AccordionTrigger">
-      <span>
-        Primer Validation
-      </span>
-    </AccordionTrigger>
-  </AccordionHeader>
-  <AccordionContent class="AccordionContent">
-    <div class="AccordionContentText">
-      The pipeline validates that primers match expected sequences. If
-      a primer sequence partially matches but fails secondary
-      validation, it is considered invalid, and the error <i>"primer
-      failed secondary validation"</i> is logged.
-    </div>
-  </AccordionContent>
-</AccordionItem>
+    <AccordionItem class="AccordionItem" value='item-4'>
+      <AccordionHeader class="AccordionHeader">
+        <AccordionTrigger class="AccordionTrigger">
+          <span>regions.csv</span>
+        </AccordionTrigger>
+      </AccordionHeader>
+      <AccordionContent class="AccordionContent">
+        <div class="AccordionContentText">
+          Associations between sequences and their identified Open Reading Frames (ORFs). 
+          <a href="https://github.com/cfe-lab/CFEIntact/blob/master/tests/expected-results-small-csv/regions.csv" target="_blank">Example.</a>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
 
-<AccordionItem class="AccordionItem" value='item-3'>
-  <AccordionHeader class="AccordionHeader">
-    <AccordionTrigger class="AccordionTrigger">
-      <span>
-        Reverse Complementation
-      </span>
-    </AccordionTrigger>
-  </AccordionHeader>
-  <AccordionContent class="AccordionContent">
-    <div class="AccordionContentText">
-      If a sequence does not pass primer detection in its current
-      form, the pipeline attempts to analyze the <u>reverse
-      complement</u> of the sequence. If primers are found in the
-      reverse complement, it is used for further analysis, and a log
-      message is generated indicating that the reverse complement was
-      used.
-    </div>
-  </AccordionContent>
-</AccordionItem>
+    <AccordionItem class="AccordionItem" value='item-5'>
+      <AccordionHeader class="AccordionHeader">
+        <AccordionTrigger class="AccordionTrigger">
+          <span>outcome_summary.csv</span>
+        </AccordionTrigger>
+      </AccordionHeader>
+      <AccordionContent class="AccordionContent">
+        <div class="AccordionContentText">
+          Pipeline summary data. Contains QC status for each sequence, etc. 
+          <!-- Example link to be updated -->
+          <a href="#" target="_blank">Example.</a>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
 
-<AccordionItem class="AccordionItem" value='item-4'>
-  <AccordionHeader class="AccordionHeader">
-    <AccordionTrigger class="AccordionTrigger">
-      <span>
-        Handling Missing Primers
-      </span>
-    </AccordionTrigger>
-  </AccordionHeader>
-  <AccordionContent class="AccordionContent">
-    <div class="AccordionContentText">
-      If either the forward or reverse primer is not found at the expected end, the sequence is tagged with errors such as:
-      <ul>
-        <li><i>"primer was not found"</i></li>
-        <li><i>"low read coverage in primer region"</i> (if the region near the end of the sequence does not have enough read coverage to reliably detect the primer)</li>
-        <li><i>"primer failed secondary validation"</i> (if a primer is found but fails a secondary validation step)</li>
-      </ul>
-    </div>
-  </AccordionContent>
-</AccordionItem>
+    <AccordionItem class="AccordionItem" value='item-6'>
+      <AccordionHeader class="AccordionHeader">
+        <AccordionTrigger class="AccordionTrigger">
+          <span>table_precursor.csv</span>
+        </AccordionTrigger>
+      </AccordionHeader>
+      <AccordionContent class="AccordionContent">
+        <div class="AccordionContentText">
+          Another pipeline summary kind of file. Contains data for sequences that passed QC. This includes final verdicts and gene regions. 
+          <!-- Example link to be updated -->
+          <a href="#" target="_blank">Example.</a>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
 
   </AccordionRoot>
 </template>
